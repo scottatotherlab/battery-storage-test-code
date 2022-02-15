992c38075f9a987901f013eb99d6b21fc09d97e7
scottatotherlab <scott@otherlab.com>
2022-02-15 10:30:13 -0800

    none

--- relaytimertestfull.py
+++ relaytimertestfull.py
@@ -1,3 +1,4 @@
+# script for Wall power / charging battery during off peak hours and battery power during on peak hours
 import RPi.GPIO as GPIO
 import datetime
 import time
@@ -15,13 +16,14 @@ clear = lambda: os.system('clear') # for clearing lines on the terminal
 try:
     while True:
         dt = datetime.datetime.now()
-        dtwom = dt.strftime("%Y-%m-%d %H:%M:%S") # datetime "dt" without milliseconds
-        if (dt.hour <= 16 or dt.hour >= 20):
-            print("Time: " + str(dtwom) + "     Wall power / charging battery")
+        dtwom = dt.strftime("%Y-%m-%d %I:%M:%S") # datetime "dt" without milliseconds
+        # if (dt.hour <= 16 or dt.hour >= 21): # example for on/off peak hours
+        if (dt.second <= 15 or dt.second >= 45): # quick example of logic
+            print("Time: " + str(dtwom) + "     Wall power / charging battery") # on peak hours: 4pm - 9pm
             GPIO.output(ChargerRelay, True)
             GPIO.output(InverterRelay, False)
         else: 
-            print("Time: " + str(dtwom) + "     Battery power")  
+            print("Time: " + str(dtwom) + "     Battery power") # off peak hours
             GPIO.output(ChargerRelay, False)
             GPIO.output(InverterRelay, True)
         time.sleep(1)
