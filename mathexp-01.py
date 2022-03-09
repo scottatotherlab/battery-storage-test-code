1ae197ede47225312ffa3340bde006a1c8f42dc3
scottatotherlab <99622081+scottatotherlab@users.noreply.github.com>
2022-03-09 15:16:10 -0800

    Delete mathexp-01.py

--- mathexp-01.py
+++
@@ -1,6 +0,0 @@
-from itertools import combinations
-sample_list = ['1','2','3','4']
-list_combinations = list()
-for n in range(len(sample_list) + 1):
-    list_combinations += list(combinations(sample_list, n))
-print(list_combinations)
